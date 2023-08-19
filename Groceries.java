844740810c66e37f50210f08cf9657f0b0eb2f9b
ThamimAlAnsari <142565222+ThamimAlAnsari@users.noreply.github.com>
2023-08-19 22:45:31 +0530

    Add files via upload

---
+++ Groceries.java
@@ -0,0 +1,10 @@
+import java.util.Scanner
+class Groceries{
+ System.out.println("*************Welcome to Fresh Veggies!*************")
+ public static void main(String[]args){
+ Groceries gr=new groceries();
+        double bill=gr.gen_Bill();
+    System.out.println("Your bill amount: "+bill);
+ }
+ System.out.println("              Thank you                            ")
+}
